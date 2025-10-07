86162a933c2f9d633fbed2883fedf0925e26cd8f
Victor-Dev8192 <victor.ribeiro.silva30@escola.pr.gov.br>
2025-10-07 09:30:37 -0300

    3qws4e5rft67gyu8hi9jok0pl´ç

---
+++ Interface/Janela.java
@@ -0,0 +1,11 @@
+package Interface;
+
+import java.awt.CardLayout;
+import javax.swing.JFrame;
+
+public class Janela extends JFrame {
+    public Janela(){
+        this.setBounds(0,0,1920,1000);
+        CardLayout ControladorTela = new CardLayout();
+    }
+}
---
+++ Teste/AppJoojs2.java
@@ -0,0 +1,10 @@
+package Teste;
+
+import javax.swing.JFrame;
+
+public class AppJoojs2 {
+    public static void main(String[] args) {
+        JFrame Janela = new JFrame();
+    }
+        Janela set;
+}
