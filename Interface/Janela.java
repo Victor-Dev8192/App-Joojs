1d21152a443cb1608595a83ff3e2f0f5fe85a322
Victor-Dev8192 <victor.ribeiro.silva30@escola.pr.gov.br>
2025-10-07 09:41:59 -0300

    §§
    §§

--- Interface/Janela.java
+++ Interface/Janela.java
@@ -2,10 +2,12 @@ package Interface;
 
 import java.awt.CardLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Janela extends JFrame {
     public Janela(){
         this.setBounds(0,0,1920,1000);
-        CardLayout ControladorTela = new CardLayout();
+        CardLayout ControleTela = new CardLayout();
+        Jpanel telas = new JPanel(ControleTela);
     }
 }
